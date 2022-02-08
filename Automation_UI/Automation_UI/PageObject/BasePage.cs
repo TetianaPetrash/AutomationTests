febf67f70a21111b2e39b4ba238cb57257064cbb
Tanya-Petrash <tetiana_petrash@epam.com>
2022-02-08 18:54:06 +0200

    add

---
+++ Automation_UI/Automation_UI/PageObject/BasePage.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+
+namespace Automation_UI.PageObject
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver driver;
+        private WebDriverWait wait;
+        private IWebElement element;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public bool SummaryDisplayed(string by)
+        {
+            try
+            {
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                element = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible((By.XPath(by))));
+                return element.Displayed;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void inputData(IWebElement element, string data)
+        {
+            element.Clear();
+            element.SendKeys(data);
+        }
+
+        internal void AnswerToReceiveUpdates(IWebElement element, string answer) 
+        {
+
+            if (answer.Equals("y"))
+            {
+                driver.FindElement((By)element).SendKeys("y");
+            }
+            else
+            {
+                driver.FindElement((By)element).SendKeys("n");
+            }
+        }
+    }
+}
